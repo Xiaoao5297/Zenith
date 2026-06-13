7f3a12ec4de9426c79e979bd4d885049f1afb358
Xiaoao5297 <2574459918@qq.com>
2026-06-13 14:14:22 +0800

    简化 CommandReader：去掉 parallel Runtime，使用主线 stream_select() 轮询 stdin

--- src/pocketmine/command/CommandReader.php
+++ src/pocketmine/command/CommandReader.php
@@ -25,34 +25,24 @@ use pocketmine\Thread;
 
 class CommandReader extends Thread{
 	private $readline;
-	/** @var resource */
-	private $readSocket = null;
-	/** @var resource */
-	private $writeSocket = null;
 	private $shutdown = false;
 	private $stdin;
-	/** @var \parallel\Runtime|null */
-	private $readerRuntime = null;
+	private $buffer = [];
 
 	public function __construct(){
 		$this->stdin = fopen("php://stdin", "r");
 		$opts = getopt("", ["disable-readline"]);
 		if(extension_loaded("readline") && !isset($opts["disable-readline"]) && (!function_exists("posix_isatty") || posix_isatty($this->stdin))){
 			$this->readline = true;
+			readline_callback_handler_install("Genisys> ", function($line){
+				if($line !== ""){
+					$this->buffer[] = $line;
+					readline_add_history($line);
+				}
+			});
 		}else{
 			$this->readline = false;
 		}
-
-		// Create socket pair for non-blocking line reading
-		$sockets = @stream_socket_pair(STREAM_PF_UNIX, STREAM_SOCK_STREAM, STREAM_IPPROTO_IP);
-		if($sockets !== false){
-			$this->readSocket = $sockets[0];
-			$this->writeSocket = $sockets[1];
-			stream_set_blocking($this->readSocket, false);
-			stream_set_blocking($this->writeSocket, false);
-		}
-
-		$this->start();
 	}
 
 	public function shutdown(){
@@ -65,81 +55,35 @@ class CommandReader extends Thread{
 	 * @return string|null
 	 */
 	public function getLine(){
-		if($this->readSocket === null or $this->readSocket === false){
-			return null;
-		}
-		$r = [$this->readSocket];
+		// Poll stdin for input (non-blocking)
+		$r = [$this->stdin];
 		$w = null;
 		$e = null;
 		if(stream_select($r, $w, $e, 0, 0) > 0){
-			$line = @fgets($this->readSocket);
-			if($line !== false and $line !== ""){
-				return trim($line);
+			if(!$this->readline){
+				$line = trim(fgets($this->stdin));
+				if($line !== ""){
+					$this->buffer[] = $line;
+				}
+			}else{
+				readline_callback_read_char();
 			}
 		}
+
+		if(!empty($this->buffer)){
+			return array_shift($this->buffer);
+		}
+
 		return null;
 	}
 
 	public function start(int $options = 0){
-		$writeSocket = $this->writeSocket;
-		$readlineMode = $this->readline;
-
-		$this->readerRuntime = new \parallel\Runtime();
-
-		$this->future = $this->readerRuntime->run(function($writeSocket, $readlineMode){
-			$stdin = fopen("php://stdin", "r");
-
-			if($readlineMode){
-				$cb = function($line) use ($writeSocket){
-					if($line !== ""){
-						@fwrite($writeSocket, $line . "\n");
-						readline_add_history($line);
-					}
-				};
-				readline_callback_handler_install("Genisys> ", $cb);
-			}
-
-			$shutdown = false;
-			while(!$shutdown){
-				$r = [$stdin];
-				$w = null;
-				$e = null;
-				if(stream_select($r, $w, $e, 0, 200000) > 0){
-					if(feof($stdin)){
-						break;
-					}
-					if(!$readlineMode){
-						$line = trim(fgets($stdin));
-						if($line !== ""){
-							@fwrite($writeSocket, $line . "\n");
-						}
-					}else{
-						readline_callback_read_char();
-					}
-				}
-			}
-
-			if($readlineMode){
-				readline_callback_handler_remove();
-			}
-			fclose($stdin);
-			@fclose($writeSocket);
-		}, [$writeSocket, $readlineMode]);
 	}
 
 	public function quit(){
 		$this->shutdown();
-		try{
-			if($this->readSocket){ @fclose($this->readSocket); $this->readSocket = null; }
-		}catch(\Throwable $e){
-		}
-		try{
-			if($this->writeSocket){ @fclose($this->writeSocket); $this->writeSocket = null; }
-		}catch(\Throwable $e){
-		}
-		try{
-			$this->readerRuntime?->close();
-		}catch(\Throwable $e){
+		if($this->readline){
+			readline_callback_handler_remove();
 		}
 	}
 
