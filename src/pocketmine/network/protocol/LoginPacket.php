647f64e5064009ed5b962aa3144cca5a9029d9b4
Xiaoao5297 <2574459918@qq.com>
2026-06-26 22:20:38 +0800

    修复 LoginPacket 协议版本验证被注释，恢复 CURRENT_PROTOCOL 检查

--- src/pocketmine/network/protocol/LoginPacket.php
+++ src/pocketmine/network/protocol/LoginPacket.php
@@ -24,10 +24,10 @@ class LoginPacket extends DataPacket{
 		$this->username = $this->getString();
 		$this->protocol1 = $this->getInt();
 		$this->protocol2 = $this->getInt();
-		/*if($this->protocol1 < Info::CURRENT_PROTOCOL){ //New fields!
-			$this->setBuffer(null, 0); //Skip batch packet handling
+		if($this->protocol1 < Info::CURRENT_PROTOCOL){
+			$this->setBuffer(null, 0);
 			return;
-		}*/
+		}
 		$this->clientId = $this->getLong();
 		$this->clientUUID = $this->getUUID();
 		$this->serverAddress = $this->getString();
