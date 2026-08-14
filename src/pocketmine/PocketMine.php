8c7aa95bca8c9080265e3a31539766deff997c08
Xiaoao5297 <2574459918@qq.com>
2026-08-14 12:16:00 +0800

    更新版本

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -72,7 +72,7 @@ namespace pocketmine {
 	use pocketmine\utils\Utils;
 	use pocketmine\wizard\Installer;
 
-	const VERSION = "0.2.3";
+	const VERSION = "0.3.0";
 	const API_VERSION = "2.0.0";
 	const CODENAME = "Safer";
 	const MINECRAFT_VERSION = "v0.14.x alpha";
